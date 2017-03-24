9ba1107ece563a0c055956f6bb2928f216fbd833
Dan Davison <(none)>
2017-03-24 13:30:21 -0700

    Average error rates over multiple replicates

--- ml/model_selection.py
+++ ml/model_selection.py
@@ -2,10 +2,11 @@ from itertools import product
 
 import pandas as pd
 
+from ml.utils import mean
 from ml.utils import random_partition
 
 
-def get_error_rates(X, y, models, n_training, n_validation):
+def get_error_rates(X, y, models, n_training, n_validation, n_replicates=1):
     """
     Return error rates computed on Cartesian product of models and training sizes.
     """
@@ -20,15 +21,19 @@ def get_error_rates(X, y, models, n_training, n_validation):
 
     rows = []
     for model, n_train in product(models, n_training):
-        print(model.serialize(), n_train)
-        _X_train, _y_train, _, _ = random_partition(X_train, y_train, n_train)
-        model.fit(_X_train, _y_train)
-        error_rate = model.get_error_rate(X_validation, y_validation)
+        error_rates = []
+        for rep in range(n_replicates):
+            _X_train, _y_train, _, _ = random_partition(X_train, y_train, n_train)
+            model.fit(_X_train, _y_train)
+            error_rate = model.get_error_rate(X_validation, y_validation)
+            error_rates.append(error_rate)
+        error_rate = mean(error_rates)
         rows.append({
             'model': model.serialize(),
             'n_train': n_train,
             'error_rate': error_rate,
         })
+        print(model.serialize(), n_train, error_rate)
 
     return pd.DataFrame(rows, columns=['model', 'n_train', 'error_rate'])
 
