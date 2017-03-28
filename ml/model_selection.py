98312743e3f776c7033f8321b3a6d5bfea264346
Dan Davison <(none)>
2017-03-28 16:48:53 -0700

    Output training errpr rate

--- ml/model_selection.py
+++ ml/model_selection.py
@@ -22,19 +22,26 @@ def get_error_rates(X, y, models, n_training, n_validation, n_replicates=1):
     rows = []
     for model, n_train in product(models, n_training):
         error_rates = []
+        training_error_rates = []
         for rep in range(n_replicates):
             _X_train, _y_train, _, _ = random_partition(X_train, y_train, n_train)
             model.fit(_X_train, _y_train)
-            error_rate = model.get_error_rate(X_validation, y_validation)
-            error_rates.append(error_rate)
+            training_error_rates.append(
+                model.get_error_rate(X_train, y_train)
+            )
+            error_rates.append(
+                model.get_error_rate(X_validation, y_validation)
+            )
         error_rate = mean(error_rates)
+        training_error_rate = mean(training_error_rates)
         rows.append({
             'model': model.serialize(),
             'n_train': n_train,
             'error_rate': error_rate,
+            'training_error_rate': training_error_rate,
         })
         print(model.serialize(), n_train, error_rate)
 
-    return pd.DataFrame(rows, columns=['model', 'n_train', 'error_rate'])
+    return pd.DataFrame(rows, columns=['model', 'n_train', 'error_rate', 'training_error_rate'])
 
 
